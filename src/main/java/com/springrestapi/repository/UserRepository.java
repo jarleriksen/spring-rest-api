27a7dba7890f6315fd42cb2c87b16e20c96bccb2
Jarl Eriksen <jarl@civitas-software.com>
2018-04-24 15:37:46 +0200

    fix gitignore

--- src/main/java/com/springrestapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springrestapi.repository;
-
-import com.springrestapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
